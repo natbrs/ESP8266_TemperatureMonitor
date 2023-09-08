5b2c2ed89b9edd37e0b3e108c025d4a67d4c8e72
Natan Almeida Ramos Rezende <83055901+natbrs@users.noreply.github.com>
2023-09-07 23:18:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express, json());
+app.use(express.json());
 
 app.get('/', (request, response)=>{
     return response.json({message:'Server is up'});
