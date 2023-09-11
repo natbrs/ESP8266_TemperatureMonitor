80809dfa715d26326c16a7843df8cc4686d7ae2d
Natan Almeida Ramos Rezende <83055901+natbrs@users.noreply.github.com>
2023-09-11 18:37:32 -0300

    Change main.hbs

--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -1,75 +1,92 @@
 #include <ESP8266WiFi.h>
-#include <DHT.h>
+#include <ESP8266HTTPClient.h>
+#include <SimpleDHT.h>
 
-const char* ssid = "SEU_SSID";
-const char* password = "SUA_SENHA"; 
-const char* server = "SEU_SERVIDOR"; 
-const int port = 3000; 
+const char* ssid = "PiNat";
+const char* password = "rafael13";
 
-DHT dht(DHTPIN, DHTTYPE);
+const char* http_site = "esp-8266-temperature-monitor-c8d4ymgxa-natbrs.vercel.app";
+const int http_port = 3000;
+const char* http_path = "/dashboard"; 
+
+int pinDHT11 = D1;
+SimpleDHT11 dht11;
+
+unsigned long previousMillis = 0;
+const long interval = 600000;
 
 void setup() {
   Serial.begin(9600);
-  delay(10);
+  Serial.println("NodeMCU - Gravando dados no BD via GET");
+  Serial.println("Aguardando conexão");
 
-  // Conecte-se à rede Wi-Fi
-  Serial.println();
-  Serial.print("Conectando a ");
-  Serial.println(ssid);
-
+  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
   }
-
-  Serial.println("");
-  Serial.println("WiFi conectado");
-
-  dht.begin();
+  Serial.println("\nWi-Fi conectado com sucesso: " + WiFi.localIP().toString());
 }
 
 void loop() {
-  delay(2000);
+  unsigned long currentMillis = millis();
 
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Falha ao ler o sensor DHT!");
-    return;
-  }
+    byte temp = 0;
+    byte humid = 0;
+    if (dht11.read(pinDHT11, &temp, &humid, NULL)) {
+      Serial.println("Falha na leitura do sensor.");
+      return;
+    }
 
-  // Enviar dados para o servidor Node.js
-  sendDataToServer(temperature, humidity);
-}
+    Serial.println("Gravando dados no BD: ");
+    Serial.print((int)temp);
+    Serial.print(" *C, ");
+    Serial.print((int)humid);
+    Serial.println(" %");
 
-void sendDataToServer(float temperature, float humidity) {
-  WiFiClient client;
-
-  if (!client.connect(server, port)) {
-    Serial.println("Falha na conexão com o servidor");
-    return;
-  }
-
-  // Construa a URL para enviar os dados
-  String url = "/add-value?temp=" + String(temperature) + "&umi=" + String(humidity);
-
-  Serial.print("Enviando dados para: ");
-  Serial.println(url);
-
-  // Envie a solicitação HTTP GET
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + server + "\r\n" +
-               "Connection: close\r\n\r\n");
-
-  while (client.connected()) {
-    if (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
+    if (!sendDataToServer((int)temp, (int)humid)) {
+      Serial.println("GET request failed");
     }
   }
+}
 
-  client.stop();
+bool sendDataToServer(int temp, int humid) {
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  String url = "https://" + String(http_site) + http_path + "?wea_humid=" + String(humid) + "&wea_temp=" + String(temp);
+  Serial.println("Placing order");
+  Serial.println(url);
+
+
+  if (client.connect(http_site, http_port)) {
+    Serial.println("Connected to client");
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + http_site + "\r\n" + "Connection: close\r\n\r\n");
+
+    while (client.connected()) {
+      Serial.println("Waiting for response from the server...");
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        break;
+      }
+    }
+
+    while (client.available()) {
+      Serial.println("esperando resto da resposta");
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
+    }
+
+    Serial.println("concluído");
+
+    client.stop();
+    return true;
+  } 
+    
+  Serial.println("Falha na conexão com o servidor");
+  return false;
 }
